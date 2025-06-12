ee72b436b05a8622aeb37a28c4b54b5f9f438ec1
Karine Débora Wagner <karinedwagner@gmail.com>
2025-06-12 16:53:47 -0300

    cria controller CandidateController

---
+++ src/main/java/br/com/karine/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,33 @@
+package br.com.karine.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.karine.gestao_vagas.exceptions.UserFoundException;
+import br.com.karine.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.karine.gestao_vagas.modules.candidate.CandidateRepository;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @PostMapping("/")
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository
+        .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+        .ifPresent((user) -> {
+            throw new UserFoundException();
+        });
+        
+        
+        return this.candidateRepository.save(candidateEntity);
+    }
+    
+}
