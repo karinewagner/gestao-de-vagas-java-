6e3bf73d1e655108a74653fa25486b2e8bebbd9d
Karine Débora Wagner <karinedwagner@gmail.com>
2025-06-12 19:08:02 -0300

    corrigi entidade job

--- src/main/java/br/com/karine/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/karine/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -26,8 +28,12 @@ public class JobEntity {
     private String benefits;
 
     @ManyToOne()
-    @JoinColumn(name = "company_id") // Por padrão o id é vinculado como chave estrangeira, mas caso quizesse outro campo era só colocar referencedColumnName = "id"
+    @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
+
+    @Column(name = "company_id")
+    @NotNull(message = "O campo companyId não pode ser null.")
+    private UUID companyId;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
