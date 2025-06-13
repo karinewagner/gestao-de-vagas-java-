4724923fcd0c90489d0dc542247179cd8301d76e
Karine Débora Wagner <karinedwagner@gmail.com>
2025-06-13 15:15:19 -0300

    padroniza as pastas de entidade e repositório do candidate

--- src/main/java/br/com/karine/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
+++ src/main/java/br/com/karine/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
@@ -1,9 +1,11 @@
-package br.com.karine.gestao_vagas.modules.candidate;
+package br.com.karine.gestao_vagas.modules.candidate.repositories;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.karine.gestao_vagas.modules.candidate.entities.CandidateEntity;
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
