557dda8fec8493245e94ccbba456608156a77017
Karine Débora Wagner <karinedwagner@gmail.com>
2025-06-17 12:37:47 -0300

    insere exemplo na requisição no swagger

--- src/main/java/br/com/karine/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/karine/gestao_vagas/modules/company/controllers/JobController.java
@@ -14,6 +14,14 @@ import br.com.karine.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.karine.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.karine.gestao_vagas.modules.company.useCases.CreateJobUseCase;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -26,7 +34,26 @@ public class JobController {
 
     @PostMapping("/")
     @PreAuthorize("hasRole('COMPANY')")
-    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+        @Tag(
+        name = "Vagas", 
+        description = "Informações das vagas"
+    )
+    @Operation(
+        summary = "Cadastro de vaga", 
+        description = "Essa função é responsável por cadastrar as vagas dentro da empresa"
+    )
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200",
+            content = {
+                @Content(schema = 
+                    @Schema(implementation = JobEntity.class)
+                )
+            }
+        )
+    })
+    @SecurityRequirement(name = "jwt_auth")
+    public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
 
         var companyId = request.getAttribute("company_id");
 
@@ -37,11 +64,6 @@ public class JobController {
             .companyId(UUID.fromString(companyId.toString()))
             .build();
 
-        try {
-            var result = this.createJobUseCase.execute(jobEntity);
-            return ResponseEntity.ok().body(result);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        return this.createJobUseCase.execute(jobEntity);
     }
 }
--- src/main/java/br/com/karine/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/karine/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,11 +1,19 @@
 package br.com.karine.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
 
+    @Schema(example = "Vaga para pessoa desenvolvedora júnior", requiredMode = RequiredMode.REQUIRED)
     private String description;
+
+    @Schema(example = "Plano de saúde e vale alimentação", requiredMode = RequiredMode.REQUIRED)
     private String benefits;
+
+    @Schema(example = "junior", requiredMode = RequiredMode.REQUIRED)
     private String level;
 }
