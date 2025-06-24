b0c64c2d916ed8f9e3a807a3ced62b44db031d7c
Karine Débora Wagner <karinedwagner@gmail.com>
2025-06-24 18:33:29 -0400

    atualiza o company entity

--- src/main/java/br/com/karine/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/br/com/karine/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -12,10 +12,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "company")
 public class CompanyEntity {
     
